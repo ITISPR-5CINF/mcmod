bb3649958638c117faeb6aa5d117dd57e0350968
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-30 16:24:08 +0200

    mcmod: Updates
    * Add placeholder logo
    * Add item used as icon
    * Add 2 methods to register blocks and items
    * Initial l18n
    
    Change-Id: I5bf870c6236d414a96bedd2aa0eb2741f89fde76

--- src/main/java/it/pr/itis/quartacinf/quartacinfmod/setup/Registration.java
+++ src/main/java/it/pr/itis/quartacinf/quartacinfmod/setup/Registration.java
@@ -1,12 +1,18 @@
 package it.pr.itis.quartacinf.quartacinfmod.setup;
 
+import java.util.function.Supplier;
 import it.pr.itis.quartacinf.quartacinfmod.QuartaCInfMod;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class Registration {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(
@@ -14,9 +20,48 @@ public class Registration {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(
             ForgeRegistries.ITEMS, QuartaCInfMod.MOD_ID);
 
-    public static void register(){
+    // L'icona che apppare nel menu di creativa
+    public static final RegistryObject<Item> ICON_ITEM = registerItem("icon_item",
+            () -> new Item(new Item.Properties()));
+
+    // La pagina che contiene tutti i nostri blocchi e oggetti custom
+    public static final CreativeModeTab TUTORIAL_TAB = new CreativeModeTab(QuartaCInfMod.MOD_ID) {
+        @Override
+        @OnlyIn(Dist.CLIENT)
+        public ItemStack makeIcon() {
+            return new ItemStack(ICON_ITEM.get());
+        }
+    };
+
+    /**
+     * Registra tutti i blocchi e gli oggetti.
+     * Questo metodo deve essere utilizzato solo dal construttore della mod.
+     */
+    public static void register() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
+    }
+
+    /**
+     * Registra un nuovo blocco
+     * @param name nome del blocco
+     * @param block oggetto Block
+     * @return
+     * @param <T>
+     */
+    public static <T extends Block> RegistryObject<T> registerBlock(final String name, final Supplier<T> block) {
+        return BLOCKS.register(name, block);
+    }
+
+    /**
+     * Registra un nuovo elemento
+     * @param name nome dell'elemento
+     * @param item oggetto Item
+     * @return
+     * @param <T>
+     */
+    private static <T extends Item> RegistryObject<T> registerItem(final String name, final Supplier<T> item) {
+        return ITEMS.register(name, item);
     }
 }
