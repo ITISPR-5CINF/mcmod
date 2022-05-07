66827302c7d6dbe828b69076f9935c1b53c467fa
Alessandro <alessandroromano2005@gmail.com>
2022-05-07 20:21:48 +0200

    mcmod: Fixed creative mode tab
    Rimossa la tab in item, ora viene utilizzata quella della classe registration in modo che ci sia solo una scheda

--- src/main/java/it/pr/itis/quartacinf/quartacinfmod/register/Items.java
+++ src/main/java/it/pr/itis/quartacinf/quartacinfmod/register/Items.java
@@ -2,6 +2,7 @@ package it.pr.itis.quartacinf.quartacinfmod.register;
 
 import java.util.function.Supplier;
 import it.pr.itis.quartacinf.quartacinfmod.QuartaCInfMod;
+import it.pr.itis.quartacinf.quartacinfmod.setup.Registration;
 import it.pr.itis.quartacinf.quartacinfmod.util.ModTier;
 import net.minecraft.world.item.*;
 import net.minecraft.world.item.crafting.RecipeType;
@@ -22,25 +23,16 @@ public class Items {
     public static final RegistryObject<Item> ICON_ITEM = registerItem("icon_item",
             () -> new Item(new Item.Properties()));
 
-    // La pagina che contiene tutti i nostri blocchi e oggetti custom
-    public static final CreativeModeTab CREATIVE_TAB = new CreativeModeTab(QuartaCInfMod.MOD_ID) {
-        @Override
-        @OnlyIn(Dist.CLIENT)
-        public ItemStack makeIcon() {
-            return new ItemStack(ICON_ITEM.get());
-        }
-    };
-
     // REGISTRATION: Init
     // Miscellaneous
     public static final RegistryObject<Item> DIAMOND_NUGGET = registerItem("diamond_nugget",
-            () -> new Item(new Item.Properties().tab(CREATIVE_TAB)));
+            () -> new Item(new Item.Properties().tab(Registration.CREATIVE_TAB)));
 
     public static final RegistryObject<Item> GOD_EYE = registerItem("god_eye",
-            () -> new BowItem(new Item.Properties().tab(CREATIVE_TAB)));
+            () -> new BowItem(new Item.Properties().tab(Registration.CREATIVE_TAB)));
 
     public static final RegistryObject<Item> COAL_COKE = registerItem("coal_coke",
-            () -> new Item(new Item.Properties().tab(CREATIVE_TAB)) {
+            () -> new Item(new Item.Properties().tab(Registration.CREATIVE_TAB)) {
                 @Override
                 public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType) {
                     return 3200;
@@ -50,30 +42,30 @@ public class Items {
     // Napoli
     //FACCIAMO RINASCERE LA SCARRAFONE MODE #PISTOLENELLAFENDI
     public static final RegistryObject<Item> NAPOLETANITE = registerItem("napoletanite",
-            () -> new Item(new Item.Properties().tab(CREATIVE_TAB)));
+            () -> new Item(new Item.Properties().tab(Registration.CREATIVE_TAB)));
 
     public static final RegistryObject<Item> NAPOLETANITE_WATCH = registerItem("napoletanite_watch",
-            () -> new Item(new Item.Properties().tab(CREATIVE_TAB)));
+            () -> new Item(new Item.Properties().tab(Registration.CREATIVE_TAB)));
 
     public static final RegistryObject<Item> NAPOLETANITE_PICKAXE = registerItem("napoletanite_pickaxe",
             () -> new PickaxeItem(ModTier.NAPOLETANITE_TIER, 1, 2.2f,
-                    new Item.Properties().tab(CREATIVE_TAB)));
+                    new Item.Properties().tab(Registration.CREATIVE_TAB)));
 
     public static final RegistryObject<Item> NAPOLETANITE_SWORD = registerItem("napoletanite_sword",
             () -> new SwordItem(ModTier.NAPOLETANITE_TIER, 3, 1.6f,
-                    new Item.Properties().tab(CREATIVE_TAB)));
+                    new Item.Properties().tab(Registration.CREATIVE_TAB)));
 
     public static final RegistryObject<Item> NAPOLETANITE_AXE = registerItem("napoletanite_axe",
             () -> new AxeItem(ModTier.NAPOLETANITE_TIER, 5, 1f,
-                    new Item.Properties().tab(CREATIVE_TAB)));
+                    new Item.Properties().tab(Registration.CREATIVE_TAB)));
 
     public static final RegistryObject<Item> NAPOLETANITE_HOE = registerItem("napoletanite_hoe",
             () -> new HoeItem(ModTier.NAPOLETANITE_TIER, 0, 4f,
-                    new Item.Properties().tab(CREATIVE_TAB)));
+                    new Item.Properties().tab(Registration.CREATIVE_TAB)));
 
     public static final RegistryObject<Item> NAPOLETANITE_SHOVEL = registerItem("napoletanite_shovel",
             () -> new ShovelItem(ModTier.NAPOLETANITE_TIER, 0, 2.8f,
-                    new Item.Properties().tab(CREATIVE_TAB)));
+                    new Item.Properties().tab(Registration.CREATIVE_TAB)));
     // REGISTRATION: END
 
     /**
